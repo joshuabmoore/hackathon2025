f5a6ca75b942f181a2755413ca9090b6062e6792
anniegbryant <anniegbryant@gmail.com>
2025-01-31 14:59:19 +1100

    Reorganize real data folders

--- run_catch22_for_TS.R
+++ run_catch22_for_TS.R
@@ -17,20 +17,10 @@ extract_features_by_problem <- function(data_path, theproblem){
                              values_var = "values", group_var = "target", 
                              feature_set = "catch22", catch24 = TRUE, seed = 123)
   
-  # Catch cases where appended NAs cause errors (i.e., different time series have different lengths)
-  # We do this by mapping over IDs to a modified feature calculation function that drops NAs by ID
-  
-  if(length(unique(tmp$id)) != length(unique(outs$id))){
-    outs <- unique(tmp$id) %>%
-      purrr::map_dfr(~ calculate_features2(tmp, id_var = "id", time_var = "timepoint", 
-                                           values_var = "values", group_var = "target", 
-                                           catch24 = TRUE, seed = 123, the_id = .x))
-  }
-  
-  save(outs, file = paste0("data/feature-calcs/", theproblem, ".Rda"))
+  # Save catch24 results
+  write.csv(catch24_res, glue("{data_path}/{theproblem}_catch24_res.csv"), row.names = F)
 }
 
 # Run the function
-
-unique(TimeSeriesData$problem)[!unique(TimeSeriesData$problem) %in% c("AllGestureWiimoteX", "AllGestureWiimoteY", "AllGestureWiimoteZ", "PLAID")] %>%
-  purrr::map(~ extract_features_by_problem(data = TimeSeriesData, theproblem = .x))
+problem_list %>%
+  purrr::map(~ extract_features_by_problem(data_path = data_path, theproblem = .x))
